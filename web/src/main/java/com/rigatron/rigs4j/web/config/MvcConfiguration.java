6ca579f8a7ed708e7659db7e013da2547c71978b
Will Rigby <williamlouisrigby@gmail.com>
2018-10-08 11:31:49 -0400

    fixed spring versioning dependency issue

--- web/src/main/java/com/rigatron/rigs4j/web/config/MvcConfiguration.java
+++ web/src/main/java/com/rigatron/rigs4j/web/config/MvcConfiguration.java
@@ -1,30 +1,14 @@
 package com.rigatron.rigs4j.web.config;
 
-import com.rigatron.rigs4j.BL.DAOs.UserDAO;
-import com.rigatron.rigs4j.BL.DAOs.UserRoleDAO;
-import com.rigatron.rigs4j.BL.DAOs.interfaces.IUserDAO;
-import com.rigatron.rigs4j.BL.DAOs.interfaces.IUserRoleDAO;
-import com.rigatron.rigs4j.BL.services.UserService;
-import com.rigatron.rigs4j.BL.services.interfaces.IUserService;
 import com.rigatron.rigs4j.web.Utilities.LayoutInterceptor;
-import org.apache.commons.dbcp.BasicDataSource;
-import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
-import org.springframework.orm.hibernate5.HibernateExceptionTranslator;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import javax.sql.DataSource;
-import java.util.Properties;
 
 @Configuration
 @EnableWebMvc
